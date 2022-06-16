4cef0d6db28284e32985a69485858d6fc429773f
MiracleXYZ <xieyuzhang_xyz@foxmail.com>
2022-06-16 17:43:33 +0800

    Update Home page.

--- 🏠_Home.py
+++ 🏠_Home.py
@@ -9,6 +9,8 @@ st.markdown(
 Some analyses based on data of the popular open-world game Genshin Impact. Made and maintained by [MiracleXYZ](https://github.com/MiracleXYZ). Hosted by [Streamlit Cloud](https://streamlit.io/).
 
 👈 Select a demo from the **sidebar** and check out the results!
+
+This work is open-source. Check our [GitHub repository](https://github.com/MiracleXYZ/genshin-network-graph) and feel free to leave comments on the [Issue page](https://github.com/MiracleXYZ/genshin-network-graph/issues)!
 """
 )
 
