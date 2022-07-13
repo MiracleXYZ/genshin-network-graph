affa7366e19942f6fbb3bae9ca344ad336a5f216
MiracleXYZ <xieyuzhang_xyz@foxmail.com>
2022-07-13 14:44:34 +0800

    Update Voice-Overs: Shikanoin Heizou, Tartaglia.

--- 🏠_Home.py
+++ 🏠_Home.py
@@ -14,6 +14,7 @@ This work is open-source. Check our [GitHub repository](https://github.com/Mirac
 
 ## Change Log
 
+- 2022-07-13: Update Voice-Overs: Shikanoin Heizou, Tartaglia.
 - 2022-07-11: Include Introduction text data. (source: Twitter [@GenshinImpact](https://twitter.com/GenshinImpact))
 - 2022-06-21: Update Voice-Overs: Kuki Shinobu.
 - 2022-06-16: Change to multi-page APP.
