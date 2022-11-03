cd29b883f438ab54426378d1263a144043e31b59
MiracleXYZ <xieyuzhang_xyz@foxmail.com>
2022-11-03 22:58:02 +0800

    Update Voice-Overs: Nahida.

--- 🏠_Home.py
+++ 🏠_Home.py
@@ -14,6 +14,7 @@ This work is open-source. Check our [GitHub repository](https://github.com/Mirac
 
 ## Change Log
 
+- 2022-11-02: Update Voice-Overs: Nahida.
 - 2022-10-14: Update Voice-Overs: Nilou.
 - 2022-09-28: Update Voice-Overs: Cyno, Candace.
 - 2022-09-09: Update Voice-Overs: Dori.
