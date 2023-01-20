77ee9516ceacf718c2b751d1847cc62f32304641
MiracleXYZ <xieyuzhang_xyz@foxmail.com>
2023-01-21 01:36:29 +0800

    Update Voice-Overs: Alhaitham, Yaoyao.

--- 🏠_Home.py
+++ 🏠_Home.py
@@ -14,6 +14,7 @@ This work is open-source. Check our [GitHub repository](https://github.com/Mirac
 
 ## Change Log
 
+- 2023-01-21: Update Voice-Overs: Alhaitham, Yaoyao.
 - 2022-12-08: Update Voice-Overs: Wanderer, Faruzan.
 - 2022-11-18: Update Voice-Overs: Layla.
 - 2022-11-02: Update Voice-Overs: Nahida.
