d6949e436837a063b85e4a33cfc8d56f8de03f29
MiracleXYZ <xieyuzhang_xyz@foxmail.com>
2023-04-10 22:38:52 +0800

    Update Voice-Overs: Kirara 🐱

--- 🏠_Home.py
+++ 🏠_Home.py
@@ -14,6 +14,7 @@ This work is open-source. Check our [GitHub repository](https://github.com/Mirac
 
 ## Change Log
 
+- 2023-04-10: Update Voice-Overs: Kirara 🐱
 - 2023-03-12: Update Voice-Overs: Dehya.
 - 2023-01-21: Update Voice-Overs: Alhaitham, Yaoyao.
 - 2022-12-08: Update Voice-Overs: Wanderer, Faruzan.
