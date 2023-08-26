31a2865ce1077470a8196d84816bddf6c67452ed
MiracleXYZ <xieyuzhang_xyz@outlook.com>
2023-08-26 21:09:57 +0800

    Update Voice-Overs.

--- 🏠_Home.py
+++ 🏠_Home.py
@@ -14,6 +14,7 @@ This work is open-source. Check our [GitHub repository](https://github.com/Mirac
 
 ## Change Log
 
+- 2023-08-26: Update Voice-Overs: Lyney, Lynette, Freminet.
 - 2023-05-24: Update Voice-Overs: Mika, Baizhu, Kaveh, Kirara.
 - 2023-03-12: Update Voice-Overs: Dehya.
 - 2023-01-21: Update Voice-Overs: Alhaitham, Yaoyao.
